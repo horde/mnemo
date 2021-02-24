7d2e2aad2a2db0410bcf1e3bf5a378593236bb6e
Ralf Lang <lang@b1-systems.de>
2021-02-24 15:38:43 +0000

    Components updated the package files.

--- lib/Application.php
+++ lib/Application.php
@@ -45,7 +45,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (5.0.0-git)';
+    public $version = '5.0.0alpha1';
 
     /**
      * Global variables defined:

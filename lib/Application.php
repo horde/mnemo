ac1ed3e2099f6cc1863210a5ff29fc6e6c275a9a
Ralf Lang <lang@b1-systems.de>
2022-11-03 08:21:46 +0000

    Development mode for mnemo-5.0.0alpha5

--- lib/Application.php
+++ lib/Application.php
@@ -45,7 +45,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (5.0.0alpha4)';
+    public $version = 'H5 (5.0.0alpha5)';
 
     /**
      * Global variables defined:

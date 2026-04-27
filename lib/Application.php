f04d627e090ee1a4b49d60f151a3aa73a5418042
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-27 22:14:42 +0200

    chore(release): bump version to 5.0.0-alpha10
    Release version 5.0.0-alpha10
    
    fix: Wrong level

--- lib/Application.php
+++ lib/Application.php
@@ -47,7 +47,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-alpha9';
+    public $version = '5.0.0-alpha10';
 
     /**
      * Global variables defined:

b0d5b685d125929961c25e0d1d1aa27454ef22f3
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-02 11:23:51 +0200

    chore(release): bump version to 5.0.0-beta4
    Release version 5.0.0-beta4
    
    docs: icon license
    style: Changed CSS and icon assets to Lucide Icons with proper attribution

--- lib/Application.php
+++ lib/Application.php
@@ -50,7 +50,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta3';
+    public $version = '5.0.0-beta4';
 
     /**
      * Global variables defined:

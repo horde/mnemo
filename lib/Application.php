140f35f33b9a5f058aa0039a993a48c1efbd5b06
Ralf Lang <ralf.lang@ralf-lang.de>
2026-06-30 21:30:42 +0200

    chore(release): bump version to 5.0.0
    Release version 5.0.0
    
    chore: update CI

--- lib/Application.php
+++ lib/Application.php
@@ -50,7 +50,7 @@ class Mnemo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = '5.0.0-beta5';
+    public $version = '5.0.0';
 
     /**
      * Global variables defined:
